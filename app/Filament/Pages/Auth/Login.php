7140589a907050ed9345b7ca5fe5f9567269b36f
cschibelsky-star <cschibelsky@gmail.com>
2026-07-09 18:20:04 -0300

    Padroniza campos do login admin Atlas

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Pages\Auth;
 
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login as BaseLogin;
 
 class Login extends BaseLogin
@@ -9,4 +12,36 @@ class Login extends BaseLogin
     protected static string $view = 'filament.pages.auth.login-atlas';
 
     protected static string $layout = 'components.layouts.auth-atlas';
+
+    protected function getEmailFormComponent(): Component
+    {
+        return TextInput::make('email')
+            ->label('Usuário ou e-mail')
+            ->hiddenLabel()
+            ->placeholder('Usuário ou e-mail')
+            ->email()
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label('Sua senha')
+            ->hiddenLabel()
+            ->placeholder('Sua senha')
+            ->password()
+            ->revealable()
+            ->required()
+            ->autocomplete('current-password')
+            ->extraInputAttributes(['tabindex' => 2]);
+    }
+
+    protected function getRememberFormComponent(): Component
+    {
+        return Checkbox::make('remember')
+            ->label('Lembrar de mim');
+    }
 }
