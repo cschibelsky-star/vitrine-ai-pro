ec535e5ccbdc6ec5cf12ee9d4fde1cafe091e148
cschibelsky-star <cschibelsky@gmail.com>
2026-07-09 16:37:44 -0300

    Usa layout standalone no login admin Atlas

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -7,4 +7,6 @@ use Filament\Pages\Auth\Login as BaseLogin;
 class Login extends BaseLogin
 {
     protected static string $view = 'filament.pages.auth.login-atlas';
+
+    protected static string $layout = 'components.layouts.auth-atlas';
 }
