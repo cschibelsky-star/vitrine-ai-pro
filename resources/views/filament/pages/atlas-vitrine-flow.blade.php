b377679f750a6a31c9df3b4cef6dcb12048ef112
cschibelsky-star <cschibelsky@gmail.com>
2026-07-10 09:33:10 -0300

    Cria view Vitrine IA Flow integrada

---
+++ resources/views/filament/pages/atlas-vitrine-flow.blade.php
@@ -0,0 +1,4 @@
+<x-filament-panels::page>
+    <style>.atlas-frame{width:100%;height:calc(100vh - 7rem);min-height:820px;border:0;border-radius:24px;background:#020617;box-shadow:0 24px 80px rgba(0,0,0,.22)}</style>
+    <iframe class="atlas-frame" src="{{ asset('prototipos/vitrine-flow-atlas-10-1.html') }}"></iframe>
+</x-filament-panels::page>
