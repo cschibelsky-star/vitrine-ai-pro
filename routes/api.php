0f7bf1e6aa1ee0700fa153b81645015b4b63edff
cschibelsky-star <cschibelsky@gmail.com>
2026-07-11 20:17:13 -0300

    Nomeia rota central de captação de leads

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\LeadCaptureController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:60,1')->group(function () {
-    Route::post('/leads', [LeadCaptureController::class, 'store']);
+    Route::post('/leads', [LeadCaptureController::class, 'store'])
+        ->name('api.leads.store');
 });
 
 require __DIR__.'/site_factory_api.php';
