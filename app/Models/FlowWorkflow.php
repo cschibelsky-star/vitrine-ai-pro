c9bc16b0c976832d2a2078984e685373ebc404e0
cschibelsky-star <cschibelsky@gmail.com>
2026-07-12 12:14:04 -0300

    feat(flow): relacionar workflows e execuções no Mission Control

--- app/Models/FlowWorkflow.php
+++ app/Models/FlowWorkflow.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FlowWorkflow extends Model
 {
@@ -51,5 +52,10 @@ class FlowWorkflow extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function executions(): HasMany
+    {
+        return $this->hasMany(FlowExecution::class, 'flow_workflow_id');
     }
 }
