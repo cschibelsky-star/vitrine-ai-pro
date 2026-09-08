6f05b1c9c171081aaad5bacd2d6d78f8d53871bb
cschibelsky-star <cschibelsky@gmail.com>
2026-09-07 22:27:22 -0300

    feat(centro-ia): register social marketing and editorial capabilities

--- config/centro_ia.php
+++ config/centro_ia.php
@@ -8,5 +8,14 @@ return [
             'agent_id' => env('CENTRO_IA_COURSE_GENERATION_AGENT_ID'),
             'agent_slug' => env('CENTRO_IA_COURSE_GENERATION_AGENT_SLUG'),
         ],
+        'social_content_generation' => [
+            'agent_slug' => env('CENTRO_IA_SOCIAL_CONTENT_AGENT_SLUG', 'marketing-ia'),
+        ],
+        'marketing_generation' => [
+            'agent_slug' => env('CENTRO_IA_MARKETING_AGENT_SLUG', 'marketing-ia'),
+        ],
+        'editorial_generation' => [
+            'agent_slug' => env('CENTRO_IA_EDITORIAL_AGENT_SLUG', 'editorial-ia'),
+        ],
     ],
 ];
