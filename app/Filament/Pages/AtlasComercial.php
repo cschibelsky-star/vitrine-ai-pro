9d323f071a840c832bd375a6b59a120b31eaf15c
cschibelsky-star <cschibelsky@gmail.com>
2026-09-14 14:34:22 -0300

    fix(factory): unify commercial navigation

--- app/Filament/Pages/AtlasComercial.php
+++ app/Filament/Pages/AtlasComercial.php
@@ -7,8 +7,8 @@ use Filament\Pages\Page;
 class AtlasComercial extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-briefcase';
-    protected static ?string $navigationGroup = '02 · Atlas Operacional';
-    protected static ?string $navigationLabel = 'Comercial';
+    protected static ?string $navigationGroup = '03 · Comercial';
+    protected static ?string $navigationLabel = 'Visão Geral';
     protected static ?string $title = 'Comercial';
     protected static ?string $slug = 'atlas/comercial';
     protected static ?int $navigationSort = 16;
