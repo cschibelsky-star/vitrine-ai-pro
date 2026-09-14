d15dffd097d60830d908cc9f2e6b7be76f4cba22
cschibelsky-star <cschibelsky@gmail.com>
2026-09-14 15:52:21 -0300

    feat(cockpit): add protected cockpit route

---
+++ routes/cockpit.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->get('/cockpit', function () {
+    $user = auth()->user();
+    abort_unless($user && $user->isAdmin(), 403);
+
+    $applications = collect(config('cockpit.applications', []))
+        ->filter(fn (array $app) => in_array($user->role ?: 'admin', $app['roles'] ?? [], true))
+        ->values();
+
+    return view('cockpit.index', compact('applications'));
+})->name('cockpit.index');
